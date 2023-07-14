3a0731b1e27c203a5309037390499c9f7b84d40e
[200~ <fawsgre@mail.ru>
2023-07-14 12:42:15 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="isPopupOpen = true">Open popup</button>
+  <message-popup
+  :is-open="isPopupOpen">
+    My someone text!
+  </message-popup>
+</template>
+
+<script>
+import MessagePopup from "./components/MessagePopup.vue"
+
+export default {
+  components: { MessagePopup },
+  data() {
+    return {
+      isPopupOpen: false
+    }
+  },
+  methods: {
+    popupConfirmed() {
+      alert("Confirmed")
+    }
+  }
+}
+</script>
---
+++ src/components/MessagePopup.vue
@@ -0,0 +1,58 @@
+<template>
+    <div v-if="isOpen" class="backdrop">
+    <div class="popup">
+      <h1>Внимание!</h1>
+      <hr />
+      <slot></slot>
+      <hr />
+      <div class="footer">
+        <button>Отмена</button>
+        &nbsp;
+        <button>Ok</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+    isOpen: {
+      type: Boolean,
+      required: true,
+    },
+  }
+}
+</script>
+
+<style>
+.popup {
+  top: 50px;
+  padding: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  position: fixed;
+  z-index: 101;
+  background-color: #1a1a1a;
+  border-radius: 10px;
+}
+
+.popup h1 {
+  text-align: center;
+  margin: 0;
+}
+
+.backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 100;
+}
+
+.footer {
+  text-align: right;
+}
+</style>
